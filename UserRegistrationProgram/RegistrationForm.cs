300247dd8cef8a4b487a33410c2c4e2e4301f0e5
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-17 16:40:22 +0530

    [Rakshit] Add: UC2_Last name entry validation

--- UserRegistrationProgram/RegistrationForm.cs
+++ UserRegistrationProgram/RegistrationForm.cs
@@ -21,7 +21,20 @@ namespace UserRegistrationProgram
             }
             else
             {
-                Console.WriteLine("Entered name is Invalid");
+                Console.WriteLine("Entered First Name is Invalid");
+            }
+
+            Regex lastName = new Regex("^[A-Z]{1}[a-z]{3,26}$");
+
+            Console.WriteLine("Enter Last Name :");
+            string lName = Console.ReadLine();
+            if (lastName.IsMatch(lName))
+            {
+                Console.WriteLine("Your last Name is :" + lName);
+            }
+            else
+            {
+                Console.WriteLine("Entered Last Name is Invalid");
             }
         }
     }
