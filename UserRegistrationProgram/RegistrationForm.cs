78e880ac8869e92c2cdaefa16fa1eb8ea1b43572
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-17 15:50:07 +0530

    [Rakshit] Add: UC1_First name entry validation

---
+++ UserRegistrationProgram/RegistrationForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class RegistrationForm
+    {
+        public void UserRegistration()
+        {
+            Regex firstName = new Regex("^[A-Z]{1}[a-z]{3,26}$");
+
+            Console.WriteLine("Enter First Name :");
+            string fName = Console.ReadLine();
+            if (firstName.IsMatch(fName))
+            {
+                Console.WriteLine("Your Name is :" + fName);
+            }
+            else
+            {
+                Console.WriteLine("Entered name is Invalid");
+            }
+        }
+    }
+}
